7d7eb2782cc75f89471c87dd989be6a1843c320c
Alex Manning <alex41290@gmail.com>
2021-09-07 22:50:01 -0400

    Set up `eslint` with `prettier` and `airbnb`

--- assets/src/App.tsx
+++ assets/src/App.tsx
@@ -1,7 +1,9 @@
-import * as React from 'react';
+import * as React from "react";
 
-export const App = () => (
+const App = () => (
   <div className="flex h-screen place-content-center place-items-center">
     <div className="text-xl p-3">Hello, person!</div>
   </div>
-)
+);
+
+export default App;
